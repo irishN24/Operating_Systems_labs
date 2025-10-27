6ea15cf2c54519465765cd862b8c50cd054523e9
irishN24 <145177386+irishN24@users.noreply.github.com>
2025-10-27 09:01:40 +0700

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+
+using namespace std;
+
+class Event {
+private:
+	mutex mutx;
+	condition_variable cv;
+	bool ready = false;
+public:
+	void producer();
+	void consumer();
+};
+void Event::producer() {
+	while (true) {
+		{
+			unique_lock<std::mutex> lock(mutx);
+			if (ready) {
+				continue;
+			}
+			ready = true;
+			cout << "Producer: The event is send!\n";
+		}
+		cv.notify_one();
+		this_thread::sleep_for(chrono::seconds(1));
+	}	
+}
+void Event::consumer() {
+	while (true) {
+		unique_lock<mutex> lock(mutx);
+		cv.wait(lock, [this] {
+			return ready;
+		});
+		ready = false;
+		cout << "Consumer: The event has been processed\n";
+	}
+}
+void producer_wrapper(Event* processor) {
+	processor->producer();
+}
+void consumer_wrapper(Event* processor) {
+	processor->consumer();
+}
+int main() {
+	Event processor;
+	thread producer_thread(producer_wrapper, &processor);
+	thread consumer_thread(consumer_wrapper, &processor);
+	producer_thread.join();
+	consumer_thread.join();
+	return 0;
+}
